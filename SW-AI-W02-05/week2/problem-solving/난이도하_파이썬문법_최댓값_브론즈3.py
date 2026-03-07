87c6283b4b3606c4157ccd8ca258f7a8a2eaff66
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-07 23:17:11 +0900

    03-07

--- SW-AI-W02-05/week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ SW-AI-W02-05/week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,22 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+
+arr = []
+max = 0
+count = 0
+for i in range(9):
+    arr.append(int(input()))
+
+for i in arr:
+    
+    if i > max:
+        max = i
+        break
+
+for i in range(9):
+    if max == arr[i]:
+        count = i + 1
+        break
+print(max)
+print(count)
