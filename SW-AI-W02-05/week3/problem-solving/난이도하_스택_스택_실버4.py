fec17350163cd17b9a2415a087f2c6dc01f5ab5b
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-14 19:59:09 +0900

    solve: boj_10828

--- SW-AI-W02-05/week3/problem-solving/난이도하_스택_스택_실버4.py
+++ SW-AI-W02-05/week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,36 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input())
+stack = []
+
+for i in range(N):
+    comment = input().split()
+
+    if comment[0] == "push":
+        stack.append(int(comment[1]))
+    
+    elif comment[0] == "pop":
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    
+    elif comment[0] == "size":
+        print(len(stack))
+    
+    elif comment[0] == "empty":
+        if stack:
+            print(0)
+        else:
+            print(1)
+    
+    elif comment[0] == "top":
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
